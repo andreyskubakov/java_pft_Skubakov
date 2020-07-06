4d2c5db91ec583ec35f2e0674f6baeca7f1a63fc
andreyskubakov <skubakov_a@mail.ru>
2020-07-06 20:34:13 +0400

    Добавлен метод для рассчета расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,32 +2,33 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 
 		hello("world");
 		hello("user");
 		hello("Andrey");
 
-		double l = 5;
-    System.out.println("Плошадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+    System.out.println("Плошадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
-
 
 	public static void hello (String somebody) {
 
     System.out.println("Hello, " + somebody + "!");
-  }
+  }*/
 
-  public static double area (double len) {
-	 return len * len;
-  }
+  public static void main(String[] args) {
 
-  public static double area (double a, double b) {
-	  return a * b;
-  }
+    Point p1 = new Point(1,2,3,4);
+    Point p2 = new Point(1,2,3,4);
+    System.out.println("Расстояние между одной точкой с координатами " + p1.x1 + " и " + p1.y2 + " и другой точкой с координатами " + p2.x2 + " и " + p2.y2 + " = " + p1.distance());
 
-}                                                                                                                                                                           
+  }
+  
+}
+
+
+
